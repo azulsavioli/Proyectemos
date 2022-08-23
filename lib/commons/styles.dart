ed796e4515dff5a102c92beef6cf1ebba8a52b7b
Maia de Oliveira <maiadeoliveira@pop-os.localdomain>
2022-08-23 00:27:50 -0300

    Criação de styles e adição do minsdk android

--- lib/commons/styles.dart
+++ lib/commons/styles.dart
@@ -8,12 +8,19 @@ abstract class ThemeText {
       color: ThemeColors.white,
       fontWeight: FontWeight.bold);
 
-  static const TextStyle paragraph16Blue = TextStyle(
+  static const TextStyle paragraph16BlueBold = TextStyle(
       fontSize: 16,
       letterSpacing: 1,
       fontFamily: 'Poppins',
       color: ThemeColors.blue,
       fontWeight: FontWeight.bold);
+
+  static const TextStyle paragraph16Gray = TextStyle(
+      fontSize: 16,
+      letterSpacing: 1,
+      fontFamily: 'Poppins',
+      color: ThemeColors.gray,
+      fontWeight: FontWeight.normal);
 
   static const TextStyle paragraph14Blue = TextStyle(
       fontSize: 16,
@@ -21,6 +28,13 @@ abstract class ThemeText {
       fontFamily: 'Poppins',
       color: ThemeColors.blue,
       fontWeight: FontWeight.bold);
+
+  static const TextStyle paragraph14Gray = TextStyle(
+      fontSize: 14,
+      letterSpacing: 1,
+      fontFamily: 'Poppins',
+      color: ThemeColors.gray,
+      fontWeight: FontWeight.normal);
 }
 
 abstract class ThemeColors {
@@ -28,4 +42,5 @@ abstract class ThemeColors {
   static const Color blue = Color.fromRGBO(0, 159, 251, 1);
   static const Color red = Color.fromRGBO(243, 1, 70, 1);
   static const Color yellow = Color.fromRGBO(254, 147, 28, 1);
+  static const Color gray = Color.fromARGB(255, 119, 118, 118);
 }
