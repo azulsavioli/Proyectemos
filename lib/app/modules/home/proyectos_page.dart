178efeb30ac794bb5e1cbb2e4d380362f169c8aa
Maia de Oliveira <maiadeoliveira@pop-os.localdomain>
2022-11-12 17:57:26 -0300

    Alteração temas e adição singlechieldScrollView

--- lib/app/modules/home/proyectos_page.dart
+++ lib/app/modules/home/proyectos_page.dart
@@ -8,12 +8,15 @@ import '../../../commons/strings.dart';
 class ProyectosPage extends StatelessWidget {
   const ProyectosPage({Key? key}) : super(key: key);
 
-  void goTo() {
-    return print('ola');
+  goTo(context, namedRoute) {
+    Navigator.pushNamed(context, namedRoute);
   }
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width * .9;
+    final height = MediaQuery.of(context).size.width * .4;
+
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -28,49 +31,53 @@ class ProyectosPage extends StatelessWidget {
             color: Color.fromRGBO(250, 251, 250, 1),
           ),
           automaticallyImplyLeading: true,
-          title: const Text(Strings.title, style: ThemeText.title20White),
+          title:
+              const Text(Strings.title, style: ThemeText.paragraph16WhiteBold),
         ),
         endDrawer: const DrawerMenuWidget(),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              CardProyecto(
-                image: Strings.imageUno,
-                title: Strings.titleCardUno,
-                titleColor: ThemeText.title20Red,
-                description: Strings.descriptionCardUno,
-                descriptionColor: ThemeText.paragraph14Gray,
-                onCallback: () => goTo,
-                backgroundColor: ThemeColors.red,
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              CardProyecto(
-                image: Strings.imageDos,
-                title: Strings.titleCardDos,
-                titleColor: ThemeText.title20Blue,
-                description: Strings.descriptionCardDos,
-                descriptionColor: ThemeText.paragraph14Gray,
-                onCallback: () => goTo,
-                backgroundColor: ThemeColors.blue,
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              CardProyecto(
-                image: Strings.imageTres,
-                title: Strings.titleCardTres,
-                titleColor: ThemeText.title20yellow,
-                description: Strings.descriptionCardTres,
-                descriptionColor: ThemeText.paragraph14Gray,
-                onCallback: () => goTo,
-                backgroundColor: ThemeColors.yellow,
-              ),
-            ],
+        body: SingleChildScrollView(
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                CardProyecto(
+                  image: Strings.imageUno,
+                  title: Strings.titleCardUno,
+                  titleColor: ThemeText.h3title22Red,
+                  description: Strings.descriptionCardUno,
+                  descriptionColor: ThemeText.paragraph14Gray,
+                  backgroundColor: ThemeColors.red,
+                  namedRoute: '/proyecto_uno',
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                CardProyecto(
+                  image: Strings.imageDos,
+                  title: Strings.titleCardDos,
+                  titleColor: ThemeText.h3title22Blue,
+                  description: Strings.descriptionCardDos,
+                  descriptionColor: ThemeText.paragraph14Gray,
+                  backgroundColor: ThemeColors.blue,
+                  namedRoute: '/proyecto_dos',
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                CardProyecto(
+                  image: Strings.imageTres,
+                  title: Strings.titleCardTres,
+                  titleColor: ThemeText.h3title22yellow,
+                  description: Strings.descriptionCardDos,
+                  descriptionColor: ThemeText.paragraph14Gray,
+                  backgroundColor: ThemeColors.yellow,
+                  namedRoute: '/proyecto_tres',
+                ),
+              ],
+            ),
           ),
         ),
       ),
