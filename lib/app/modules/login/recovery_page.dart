afa9bd0c542902cb9c3748e10dd09cc436b7ea8b
Maia de Oliveira <maiadeoliveira@pop-os.localdomain>
2022-11-12 17:56:01 -0300

    Adição pagina de recuperação de senha

---
+++ lib/app/modules/login/recovery_page.dart
@@ -0,0 +1,150 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:proyectemos/commons/styles.dart';
+
+class RecoveryPasswordPage extends StatefulWidget {
+  const RecoveryPasswordPage({Key? key}) : super(key: key);
+
+  @override
+  State<RecoveryPasswordPage> createState() => _RecoveryPasswordPageState();
+}
+
+class _RecoveryPasswordPageState extends State<RecoveryPasswordPage> {
+  final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+
+  late Future<bool> result;
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: ThemeColors.white,
+        appBar: AppBar(
+          backgroundColor: ThemeColors.blue,
+          elevation: 0,
+          title: const Text(
+            "Recuperar contraseña",
+            style: ThemeText.paragraph16White,
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Center(
+                  child: Text(
+                    'Reciba un correo electrónico para\nrestablecer tu contraseña',
+                    style: ThemeText.paragraph16GrayNormal,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(
+                  height: 60,
+                ),
+                TextFormField(
+                  style: ThemeText.paragraph16GrayNormal,
+                  controller: emailController,
+                  cursorColor: Colors.blueGrey,
+                  decoration: const InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blueGrey),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                    label: Padding(
+                      padding: EdgeInsets.only(left: 8.0),
+                      child: Text(
+                        'Correo electrónico',
+                        style: ThemeText.paragraph16GrayLight,
+                      ),
+                    ),
+                  ),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (email) {
+                    if (email!.isEmpty) {
+                      return 'Ingrese el correo electrónico correctamente';
+                    }
+                    if (!email.contains('@')) {
+                      return 'Ingrese un correo eletrónico valido';
+                    }
+                    if (email.length < 10) {
+                      return 'El correo electrónico debe tener al menos 10 caracteres';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                  height: 60,
+                  child: ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      primary: ThemeColors.yellow,
+                      minimumSize: const Size.fromHeight(50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    onPressed: () => {
+                      if (formKey.currentState!.validate())
+                        {
+                          resetPassword.call().then(
+                                (value) =>
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text(
+                                        'Correo electrónico de recuperación de contraseña enviado'),
+                                  ),
+                                ),
+                              ),
+                          Navigator.pop(context)
+                        }
+                      else
+                        {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text(
+                                  '¡Ups! Ha ocurrido un error y su correo electrónico de recuperación de contraseña no ha sido enviado. ¡Inténtalo de nuevo!'),
+                            ),
+                          ),
+                        }
+                    },
+                    icon: const Icon(Icons.email_outlined),
+                    label: const Text(
+                      'Restablecer la contraseña',
+                      style: ThemeText.paragraph16WhiteBold,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+
+  Future resetPassword() async {
+    try {
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: ((context) =>
+            const Center(child: CircularProgressIndicator())),
+      );
+      await FirebaseAuth.instance
+          .sendPasswordResetEmail(email: emailController.text.trim());
+    } on FirebaseAuthException catch (e) {
+      print(e);
+    }
+  }
+}
