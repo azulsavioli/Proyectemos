a4e2a0b0e1d34ae7dc3a6119b6e0efec66938537
Maia de Oliveira <fernandamaiadeoliveira@gmail.com>
2023-01-10 22:16:14 -0300

    Criação do repository

--- lib/app/proyectemos_repository.dart
+++ lib/app/proyectemos_repository.dart
@@ -1,8 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:proyectemos/app/services/auth_services.dart';
-import 'package:proyectemos/services/firebase/db_firestore_helper.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'services/firebase/db_firestore_helper.dart';
 
 class ProyectemosRepository extends ChangeNotifier {
   late FirebaseFirestore db;
@@ -22,7 +22,7 @@ class ProyectemosRepository extends ChangeNotifier {
     db = DBFirestore.get();
   }
 
-  saveAnswers(doc, answer) async {
+  Future saveAnswers(doc, answer) async {
     sharedPreferences = await SharedPreferences.getInstance();
     studentInfo = sharedPreferences.getString('studentInfo');
     await db
@@ -31,6 +31,28 @@ class ProyectemosRepository extends ChangeNotifier {
         .set(answer);
 
     notifyListeners();
+  }
+
+  Future getAnswers(doc) async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    studentInfo = sharedPreferences.getString('studentInfo');
+    List tasksAnswers = [];
+
+    final DocumentReference document = db
+        .collection("educandos/$studentInfo/${authService.userAuth!.uid}")
+        .doc("/$doc");
+
+    try {
+      await document.get().then((snapshot) {
+        if (snapshot.data() != null) {
+          tasksAnswers.add(snapshot.data());
+        }
+      });
+    } on FirebaseException catch (e) {
+      return e.toString();
+    }
+    notifyListeners();
+    return tasksAnswers;
   }
 
   removeAnswers(doc) async {
