0112fad30f659e35246dc94d1d36abe9630312bf
Maia de Oliveira <fernandamaiadeoliveira@gmail.com>
2023-01-22 14:44:45 -0300

    adicao base pagi tres e quatro artistas

---
+++ lib/app/modules/proyecto_uno/artistas_latinoamericanos/tarea_cuatro.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PUnoArtistasLatinoamericanosTareaCuatroPage extends StatefulWidget {
+  const PUnoArtistasLatinoamericanosTareaCuatroPage({super.key});
+
+  @override
+  State<PUnoArtistasLatinoamericanosTareaCuatroPage> createState() =>
+      _PUnoArtistasLatinoamericanosTareaCuatroPageState();
+}
+
+class _PUnoArtistasLatinoamericanosTareaCuatroPageState
+    extends State<PUnoArtistasLatinoamericanosTareaCuatroPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/app/modules/proyecto_uno/artistas_latinoamericanos/tarea_tres.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PUnoArtistasLatinoamericanosTareaTresPage extends StatefulWidget {
+  const PUnoArtistasLatinoamericanosTareaTresPage({super.key});
+
+  @override
+  State<PUnoArtistasLatinoamericanosTareaTresPage> createState() =>
+      _PUnoArtistasLatinoamericanosTareaTresPageState();
+}
+
+class _PUnoArtistasLatinoamericanosTareaTresPageState
+    extends State<PUnoArtistasLatinoamericanosTareaTresPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
