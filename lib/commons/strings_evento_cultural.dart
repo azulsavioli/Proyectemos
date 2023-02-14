a623167044ea990d91c52a65324c517bbae6b226
Maia de Oliveira <fernandamaiadeoliveira@gmail.com>
2023-02-13 23:14:39 -0300

    Criação do arquivo strings evento

---
+++ lib/commons/strings_evento_cultural.dart
@@ -0,0 +1,22 @@
+abstract class StringsEventoCultural {
+// ProyectosPage
+
+  //Evento Cultural
+
+  //Descrição Page tarea uno
+  static const String descriptionOneEventocultural = '''
+  En grupos, van a elegir un artista de algún país hispánoamericano para crear un evento en su país de origen.\n
+  Por ejemplo, pueden crear una exposición de pinturas del artista en algún museo de su ciudad de origen.\n
+  Para eso es importante que tengan en cuenta algunos de los seguientes puntos:
+ ''';
+
+  static const String descriptionTwoEventocultural = '''
+  Después que decidan sobre estos puntos se lo presenten a la profesora.\n
+ Deben enviar la propuesta del evento de manera escrita(imágen, slide o texto) y oral (audio).\n
+  Despúes, la profesora les dará la devolución si el evento está apto para ser creado.
+ ''';
+
+  //Descrição Page dos
+  static const String descriptionDosEventocultural = '''
+En este espacio vas a recibir el feedback de la profesora sobre el evento creado por tu grupo.\n\nAdemas de eso, van a poder tener contacto directo con la profesora en el caso que tengan dudas o si necesiten de una mediación.''';
+}
