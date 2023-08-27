1b2eeaafc813a58f3d1276971287fcfcf97328dd
Maia de Oliveira <fernandamaiadeoliveira@gmail.com>
2023-08-27 17:36:57 -0300

    Adição controler pagina contato professora

--- lib/app/modules/teacher_contact/teacher_controller.dart
+++ lib/app/modules/teacher_contact/teacher_controller.dart
@@ -1,58 +1,51 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:provider/provider.dart';
 
-import '../../../repository/proyectemos_repository.dart';
+import '../../../repository/repository_impl.dart';
 import '../../../utils/email_sender.dart';
 
 class TeacherController {
   final BuildContext context;
 
   TeacherController(this.context);
-  final _repository = ProyectemosRepository();
-
-  Future<List> getEmailTeacherFromFirebase() async {
-    final emails = [];
-    final repository = context.read<ProyectemosRepository>();
-
-    try {
-      final data = await repository.getTeacherEmail();
-      emails.addAll(data);
-    } on FirebaseException catch (e) {
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text(e.toString())));
-    }
-    return emails;
-  }
+  final _repository = RepositoryImpl();
 
   Future<void> sendEmail(
     GoogleSignInAccount currentUser,
     String subject,
     String description,
   ) async {
-    final email = await getEmailTeacherFromFirebase();
-    final studentInfo = await _repository.getUserInfo();
-    final studentInformation = studentInfo.split('/');
+//    final email = await getEmailTeacherFromFirebase();
+//    final studentInfo = await _repository.getUserInfo();
+//    final studentInformation = studentInfo.split('/');
 
-    final allStudentInfo = [
-      studentInformation[0],
-      studentInformation[1],
-      studentInformation[2]
-    ];
+//    final allStudentInfo = [
+//      studentInformation[0],
+//      studentInformation[1],
+//      studentInformation[2]
+//    ];
 
-    final text = '''
+    final emailSender = EmailSender();
+
+//    await emailSender.sendEmailToTeacher(
+//      currentUser,
+    //    [],
+//      [email.first.values.first],
+    //  subject,
+    //    text,
+//    );
+//  }
+
+    String createEmailMessage(
+      List<String> allStudentInfo,
+      List<String> respostas,
+    ) {
+      final text = '''
 Proyectemos\n
 ${allStudentInfo[0]} - ${allStudentInfo[1]} - ${allStudentInfo[2]}\n\n 
 $description''';
-    final emailSender = EmailSender();
 
-    await emailSender.sendEmailToTeacher(
-      currentUser,
-      [],
-      [email.first.values.first],
-      subject,
-      text,
-    );
+      return text;
+    }
   }
 }
